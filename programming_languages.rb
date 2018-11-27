e8963edca200cd8051f4bb4257b23af0b8d55a16
Wil Cook <wil.cook.is@gmail.com>
2018-11-27 16:21:55 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  # binding.pry
+  new_hash = {}
+  languages.map do |style, language|
+    # binding.pry
+    language.map do |key, value|
+      new_hash.merge!({key =>{:type => "", :style => []}})
+      new_hash[key][:type] = value[:type]
+      # binding.pry
+       new_hash[key][:style] << style
+    end
+    new_hash[:javascript][:style] << :oo
+    # binding.pry
+  end
+  new_hash
 end
