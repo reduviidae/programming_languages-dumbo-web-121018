02bda435dbda1b04c2abcc604e586dde94524189
Wil Cook <wil.cook.is@gmail.com>
2018-11-27 16:10:27 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,8 +8,10 @@ def reformat_languages(languages)
     language.map do |key, value|
       new_hash.merge!({key =>{:type => "", :style => []}})
       new_hash[key][:type] = value[:type]
-      new_hash[key][:style] << style
-      binding.pry
+      # binding.pry
+    end
+    style.map do |element|
+       new_hash[key][:style] << element
     end
     # binding.pry
   end
