68f17c7ea93e82ab3ccf0f89b9a57c666b82b7e9
Wil Cook <wil.cook.is@gmail.com>
2018-11-27 16:07:03 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  # binding.pry
+  new_hash = {}
+  languages.map do |style, language|
+    # binding.pry
+    language.map do |key, value|
+      new_hash.merge!({key =>{:type => "", :style => []}})
+      new_hash[key][:type] = value[:type]
+      new_hash[key][:style] << style
+      # binding.pry
+    end
+    # binding.pry
+  end
+  new_hash
 end
